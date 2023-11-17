9cee016c8fa15150c7d132127e110cf31d693ad6
Mederic Roy Beldia <twocngdagz@yahoo.com>
2023-11-17 14:33:57 +0800

    add test for achievement unlock event

---
+++ tests/Unit/AchievementUnlockedEventTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Events\AchievementUnlocked;
+use App\Models\Achievement;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Event;
+use Tests\TestCase;
+
+class AchievementUnlockedEventTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testAchievementUnlockedEvent(): void
+    {
+        Event::fake();
+        $user = User::factory()->create();
+        //Unlock the achievement for the user
+        $user->unlockAchievement('First Lesson Watched');
+
+        Event::assertDispatched(AchievementUnlocked::class, function ($event) use ($user) {
+            return $event->name === 'First Lesson Watched' && $event->user->id === $user->id;
+        });
+    }
+}
